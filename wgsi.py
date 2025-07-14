0461a1827f5ad30fa15401faeb7991e8a74a87f8
sharkmaster34 <bridielizvalentine@gmail.com>
2025-07-14 20:05:43 +1000

    Actually add wsgi.py

--- wgsi.py
+++ wgsi.py
@@ -1,8 +1,3 @@
-# wsgi.py
 from myclinic import create_app
 
-app = create_app()      # ← builds and exports the Flask instance
-
-# (Optional) Only runs locally if you call `python wsgi.py`
-if __name__ == "__main__":
-    app.run(debug=True)
+app = create_app()
